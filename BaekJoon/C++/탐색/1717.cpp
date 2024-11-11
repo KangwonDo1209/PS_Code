a335d52d4df612365657f4fd7863e1f123e80d07
KangwonDo1209 <ehtjd1209@gmail.com>
2024-11-12 05:23:00 +0900

    no message

---
+++ BaekJoon/C++/탐색/1717.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+using namespace std;
+typedef int num;
+
+void _union(num a, num b);
+num _find(num a);
+bool _check(num a, num b);
+
+vector<num> parent;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(0);
+	cin.tie(0);
+
+	num n, m;
+	cin >> n >> m;
+	num k, a, b;
+	parent.resize(n + 1);
+	for (num i = 0; i <= n; i++) {
+		parent[i] = i;
+	}
+	while (m--) {
+		cin >> k >> a >> b;
+		if (k == 0) _union(a, b);
+		else cout << (_check(a, b) ? "YES" : "NO") << "\n";
+	}
+}
+
+void _union(num a, num b) {
+	num ap = _find(a);
+	num bp = _find(b);
+	if (ap != bp) parent[bp] = ap;
+}
+num _find(num a) {
+	if (a == parent[a]) return a;
+	else return parent[a] = _find(parent[a]);
+}
+bool _check(num a, num b) {
+	num ap = _find(a);
+	num bp = _find(b);
+	return ap == bp;
+}
+
---
+++ BaekJoon/C++/탐색/1753.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+using namespace std;
+typedef int num;
+num MAX = 2100000000;
+typedef pair<num, num> edge; // first는 가중치, second는 노드 번호
+num V, E, K;
+vector<num> m_distance;
+vector<bool> visited;
+vector<vector<edge>> m_list;
+priority_queue<edge, vector<edge>, greater<edge>> pq;
+
+void _dijkstra();
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(0);
+	cin.tie(0);
+
+	cin >> V >> E >> K;
+	m_distance.resize(V + 1);
+	fill(m_distance.begin(), m_distance.end(), MAX);
+
+	visited.resize(V + 1);
+	fill(visited.begin(), visited.end(), false);
+	m_list.resize(V + 1);
+
+	num u, v, w;
+	for (num i = 0; i < E; i++) {
+		cin >> u >> v >> w;
+		m_list[u].push_back(edge(w, v));
+	}
+	_dijkstra();
+	for (num i = 1; i <= V; i++) {
+		if (visited[i]) cout << m_distance[i] << "\n";
+		else cout << "INF\n";
+	}
+}
+
+void _dijkstra() {
+	pq.push(edge(0, K));
+	m_distance[K] = 0;
+	while (!pq.empty()) {
+		edge current = pq.top();
+		pq.pop();
+		num c_v = current.second;
+		if (visited[c_v]) continue;
+		visited[c_v] = true;
+		for (num i = 0; i < m_list[c_v].size(); i++) {
+			edge tmp = m_list[c_v][i];
+			num next = tmp.second;
+			num value = tmp.first;
+			if (m_distance[next] > m_distance[c_v] + value) {
+				m_distance[next] = value + m_distance[c_v];
+				pq.push(edge(m_distance[next], next));
+			}
+		}
+	}
+}
